f52aa9eb30c3a118af9791495beff9ba2484c55f
Gonçalo Ramalho <goncaloramalho@Goncalos-iMac-New.local>
2018-06-13 16:43:56 +0200

    Set propTypes, Import data from API
    - Add Proptypes to project
    - Set propTypes in Movie
    - Import data with async fetch
    - Setmore data in Movie Component

--- src/App.js
+++ src/App.js
@@ -4,26 +4,34 @@ import './App.css';
 
 import Movie from "./Movie";
 
-const movies = [
-  {
-    id: 1,
-    title: 'Star Wars'
-  },
-  {
-    id: 2,
-    title: 'La Cucaratxa Borracha'
-  },
-  {
-    id: 3,
-    title: 'Deadpool'
-  },
-  {
-    id: 4,
-    title: 'Say what?!'
-  },
-];
-
 class App extends Component {
+
+  /**
+   * Set default state
+   */
+  state = {
+    movies: []
+  };
+
+  /**
+   * Define default values for props that arent required
+   */
+  async componentDidMount() {
+    try {
+      // Api call
+      const result = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=b01d116084668e4b15d36351e4941996&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+
+      // Wait for json to be ready and save it in variable
+      const movies = await result.json();
+
+      // Update state with api results
+      this.setState({
+        movies: movies.results
+      })
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
   render() {
     return (
@@ -33,7 +41,7 @@ class App extends Component {
           <h1 className="App-title">Movie database</h1>
         </header>
         {/*Map overt each movie an output movie component*/}
-        {movies.map( movie => <Movie key={movie.id} movie={movie} /> )}
+        {this.state.movies.map( movie => <Movie key={movie.id} movie={movie} description={movie.overview} /> )}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Movie extends Component {
+
+  /**
+   * Define propTypes for our props
+   * Notes: Should always have "isRequired" or a default value
+   * @type {{movie}}
+   */
+  static propTypes = {
+    movie: PropTypes.shape({
+      title: PropTypes.string.isRequired
+    })
+  };
+
+  /**
+   * Define default values for props that arent required
+   */
+  static defaultProps = {
+    description: 'No description for this'
+  }
 
   render() {
     const { title } = this.props.movie;
     return (
       <div>
-        {title}
+        <h3>{title}</h3>
+        <p>{this.props.description}</p>
       </div>
     )
   }

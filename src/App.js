964f17387249d33e15a496f9f55eae097339a2a9
Gonçalo Ramalho <goncaloramalho@Goncalos-iMac-New.local>
2018-06-13 15:32:12 +0200

    Start structure
    - Add logo
    - Create Movie component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Movie from "./Movie";
+
+const movies = [
+  {
+    id: 1,
+    title: 'Star Wars'
+  },
+  {
+    id: 2,
+    title: 'La Cucaratxa Borracha'
+  },
+  {
+    id: 3,
+    title: 'Deadpool'
+  },
+  {
+    id: 4,
+    title: 'Say what?!'
+  },
+];
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Movie database</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/*Map overt each movie an output movie component*/}
+        {movies.map( movie => <Movie key={movie.id} movie={movie} /> )}
       </div>
     );
   }
---
+++ src/Movie.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Movie extends Component {
+
+  render() {
+    const { title } = this.props.movie;
+    return (
+      <div>
+        {title}
+      </div>
+    )
+  }
+}
